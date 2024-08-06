2b5883bbfd4370ae1998abad50aeca5cbee0448b
zahmed <zohaib0718@gmail.com>
2024-08-06 10:56:25 -0500

    type check bypass

--- src/components/magicui/blur-fade.tsx
+++ src/components/magicui/blur-fade.tsx
@@ -17,6 +17,7 @@ interface BlurFadeProps {
   inViewMargin?: string;
   blur?: string;
 }
+
 const BlurFade = ({
   children,
   className,
@@ -29,7 +30,7 @@ const BlurFade = ({
   blur = "6px",
 }: BlurFadeProps) => {
   const ref = useRef(null);
-  const inViewResult = useInView(ref, { once: true, margin: inViewMargin });
+  const inViewResult = useInView(ref, { once: true, margin: inViewMargin as any });
   const isInView = !inView || inViewResult;
   const defaultVariants: Variants = {
     hidden: { y: yOffset, opacity: 0, filter: `blur(${blur})` },
